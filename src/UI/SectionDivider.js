1a9132c744326d7f2895bc78eee9dc8ca54936ff
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-27 15:55:30 -0400

    rename LineBreak.js to SectionDivider.js

---
+++ src/UI/SectionDivider.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../stylesheets/SectionDivider.css";
+
+const SectionDivider = () => {
+  return <div className="line"></div>;
+};
+
+export default SectionDivider;
