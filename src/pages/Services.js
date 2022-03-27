ea42147ab03805ef855ecf354dd9c05aa1313364
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-27 15:51:00 -0400

    create Services component

---
+++ src/pages/Services.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Main from "../UI/Main";
+
+const Services = () => {
+  return (
+    <Fragment>
+      <Header />
+      <Main>
+        <h1>Services Page</h1>
+      </Main>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Services;
