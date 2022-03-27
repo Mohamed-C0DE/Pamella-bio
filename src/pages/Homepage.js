2a1928247def1df692d7014325c57a4ed0779227
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-27 15:46:30 -0400

    import and use LineBreak and Footer component

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,9 @@
 import { Fragment } from "react";
 import Header from "../components/Header";
 import Main from "../UI/Main";
+import LineBreak from "../UI/LineBreak";
 import "../stylesheets/Homepage.css";
+import Footer from "../components/Footer";
 
 const Homepage = () => {
   return (
@@ -9,7 +11,9 @@ const Homepage = () => {
       <Header />
       <Main className="homepage">
         <div className="jumbotron"></div>
+        <LineBreak />
       </Main>
+      <Footer />
     </Fragment>
   );
 };
