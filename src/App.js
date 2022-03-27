79c81c1a74d739dc2ac19c61051d1808d8aaad4e
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-27 15:41:36 -0400

    import About, Services, Books, Community components and add route for each component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Books from "./pages/Books";
+import Community from "./pages/Community";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/community" element={<Community />} />
       </Routes>
     </Router>
   );
