62385d0607c4f8ab9bce7b0c5f82336745ef4350
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-27 15:45:40 -0400

    create Community component

---
+++ src/pages/Community.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Main from "../UI/Main";
+
+const Community = () => {
+  return (
+    <Fragment>
+      <Header />
+      <Main>
+        <h1>Community Page</h1>
+      </Main>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Community;
