01046a40e7431614176931007b95e024a1fa4076
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-29 16:59:13 -0400

    create Websites component

---
+++ src/pages/Websites.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Main from "../UI/Main";
+
+const Websites = () => {
+  return (
+    <Fragment>
+      <Header />
+      <Main>
+        <h1>Website Page</h1>
+      </Main>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Websites;
