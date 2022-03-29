5ea43d72ca9f7dc157bbffd5a347fbc15bae01ca
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-29 16:58:55 -0400

    rename Services component to PublicSpeaking

---
+++ src/pages/PublicSpeaking.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Main from "../UI/Main";
+
+const Services = () => {
+  return (
+    <Fragment>
+      <Header />
+      <Main>
+        <h1>Public Speaking Page</h1>
+      </Main>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Services;
