d4f51fed08c1c77db93c7f839a00fe6fd9a2582b
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-29 22:22:35 -0400

    add row with 2 columns, one with an image and the other with text

--- src/pages/PublicSpeaking.js
+++ src/pages/PublicSpeaking.js
@@ -2,13 +2,24 @@ import React, { Fragment } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../UI/Main";
+import "../stylesheets/PublicSpeaking.css";
 
 const Services = () => {
   return (
     <Fragment>
       <Header />
       <Main>
-        <h1>Public Speaking Page</h1>
+        <div className="speaking">
+          <div className="speaking-row">
+            <div className="column">
+              <img src="https://images.unsplash.com/photo-1574802406791-ef6898f311d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=382&q=80" />
+            </div>
+            <div className="column">
+              <h3>Hosting An Event About Entrepreneurship or Startups?</h3>
+              <p>Look no further</p>
+            </div>
+          </div>
+        </div>
       </Main>
       <Footer />
     </Fragment>
