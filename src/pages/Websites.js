e8b429343a3dbc40c9f32da005eca324757b3cd1
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-30 16:17:54 -0400

    add rel=noreferrer to a elements

--- src/pages/Websites.js
+++ src/pages/Websites.js
@@ -15,34 +15,50 @@ const Websites = () => {
           <h3>Websites</h3>
           <div className="websites-gallery">
             <div className="column">
-              <a href="https://dankbeautyco.com/" target="_blank">
+              <a
+                href="https://dankbeautyco.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <div className="img img1">
                   <p>DANK BEAUTY</p>
                 </div>
               </a>
-              <a href="https://pamohherbs.com/home/" target="_blank">
+              <a
+                href="https://pamohherbs.com/home/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <div className="img img2">
                   <p>PAM OH HERBS</p>
                 </div>
               </a>
-              <a href="https://moggpam.com/" target="_blank">
+              <a href="https://moggpam.com/" target="_blank" rel="noreferrer">
                 <div className="img img3">
                   <p>MOGGPAM</p>
                 </div>
               </a>
             </div>
             <div className="column">
-              <a href="https://dankluxeproperties.com/" target="_blank">
+              <a
+                href="https://dankluxeproperties.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <div className="img img4">
                   <p>DANK LUXE PROPERTIES</p>
                 </div>
               </a>
-              <a href="https://myorganicchef.com/" target="_blank">
+              <a
+                href="https://myorganicchef.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <div className="img img5">
                   <p>MY ORGANIC CHEF</p>
                 </div>
               </a>
-              <a href="https://pamskids.com/" target="_blank">
+              <a href="https://pamskids.com/" target="_blank" rel="noreferrer">
                 <div className="img img6">
                   <p>PAMS KIDS</p>
                 </div>
