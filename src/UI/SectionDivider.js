bac12c20b43c3ebffff49dd81bb737fe7d821a40
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-30 15:53:05 -0400

    not being used

--- src/UI/SectionDivider.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../stylesheets/SectionDivider.css";
-
-const SectionDivider = () => {
-  return <div className="line"></div>;
-};
-
-export default SectionDivider;
