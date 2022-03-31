0b84a83a096829d8e136400a5e8fa7c8f639bf14
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-31 13:49:01 -0400

    change websites js component and css file to commercial

---
+++ src/pages/Commercial.js
@@ -0,0 +1,75 @@
+import React, { Fragment } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import ScrollToTop from "../components/ScrollToTop";
+import Main from "../UI/Main";
+import "../stylesheets/Commercial.css";
+
+const Commercial = () => {
+  return (
+    <Fragment>
+      <ScrollToTop />
+      <Header />
+      <Main>
+        <div className="commercial">
+          <h3>Commercial</h3>
+          <div className="commercial-gallery">
+            <div className="column">
+              <a
+                href="https://dankbeautyco.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="img img1">
+                  <p>DANK BEAUTY</p>
+                </div>
+              </a>
+              <a
+                href="https://pamohherbs.com/home/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="img img2">
+                  <p>PAM OH HERBS</p>
+                </div>
+              </a>
+              <a href="https://moggpam.com/" target="_blank" rel="noreferrer">
+                <div className="img img3">
+                  <p>MOGGPAM</p>
+                </div>
+              </a>
+            </div>
+            <div className="column">
+              <a
+                href="https://dankluxeproperties.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="img img4">
+                  <p>DANK LUXE PROPERTIES</p>
+                </div>
+              </a>
+              <a
+                href="https://myorganicchef.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="img img5">
+                  <p>MY ORGANIC CHEF</p>
+                </div>
+              </a>
+              <a href="https://pamskids.com/" target="_blank" rel="noreferrer">
+                <div className="img img6">
+                  <p>PAMS KIDS</p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Main>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Commercial;
